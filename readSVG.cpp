9056921e9f6c0d31a5fa0f5204d9287b0a368240
Vasco Alves <167411063+Valves734@users.noreply.github.com>
2024-05-14 17:28:08 +0100

    readsvg.cpp acabar a leitura de todas as figuras geometricas

--- readSVG.cpp
+++ readSVG.cpp
@@ -3,6 +3,7 @@
 #include "SVGElements.hpp"
 #include "Color.hpp"
 #include "external/tinyxml2/tinyxml2.h"
+#include <sstream>
 
 using namespace std;
 using namespace tinyxml2;
@@ -29,8 +30,37 @@ namespace svg
             } else if (strcmp(child->Name(), "circle") == 0) {
                 Circle* circle_object = new Circle(parse_color(child->Attribute("fill")), {child->IntAttribute("cx"), child->IntAttribute("cy")}, child->IntAttribute("r"));
                 svg_elements.push_back(circle_object);
+            } else if (strcmp(child->Name(), "polyline") == 0) {
+                istringstream iss(child->Attribute("points"));
+                vector<Point> polypontos;
+                Point temp;
+                while (iss >> temp.x ){
+                    char virgula;
+                    iss >> virgula;
+                    iss >> temp.y;
+                    polypontos.pushback(temp);
+                }
+                Polyline* polyline_object = new Polyline(polypontos,parse_color(child->Attribute("stroke")) );
+                svg_elements.push_back(polyline_object);
+            } else if (strcmp(child->Name(), "line") == 0) {
+                Line* line_object = new Line({child->IntAttribute("x1"),child->IntAttribute("y1")},{child->IntAttribute("x2"),child->IntAttribute("y2")},parse_color(child->Attribute("stroke")));
+                svg_elements.push_back(line_object);
+            } else if (strcmp(child->Name(), "polygon") == 0) {
+                istringstream iss(child->Attribute("points"));
+                vector<Point> polypontos;
+                Point temp;
+                while (iss >> temp.x ){
+                    char virgula;
+                    iss >> virgula;
+                    iss >> temp.y;
+                    polypontos.pushback(temp);
+                }
+                Polygon* polygon_object = new Polygon(polypontos,parse_color(child->Attribute("fill")));
+                svg_elements.push_back(polygon_object);
+            } else if (strcmp(child->Name(), "rect") == 0) {
+                Rect* rect_object = new Rect({child->IntAttribute("x"),child->IntAttribute("y")},child->IntAttribute("width"),child->IntAttribute("height"),parse_color(child->Attribute("fill")));
+                svg_elements.push_back(rect_object);
             }
-        }
-        
     }
+}
 }
